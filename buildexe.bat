ea44cefd02031087ecaa4936a097b10c0129a559
Dion Timmer <diontimmer@live.nl>
2023-01-17 15:41:21 -0500

    fix numpy

--- buildexe.bat
+++ buildexe.bat
@@ -1,1 +1,1 @@
-pyinstaller --noconfirm --onefile --windowed --icon "%~dp0data/dtico.ico" --add-data "%~dp0data;data" --clean --distpath "%~dp0/binaries"  "%~dp0ABS.py"
+pyinstaller --noconfirm --onefile --windowed --icon "%~dp0data/dtico.ico" --add-data "%~dp0data;data" --clean --distpath "%~dp0/binaries"  "%~dp0ABS.py" --hidden-import=numpy
